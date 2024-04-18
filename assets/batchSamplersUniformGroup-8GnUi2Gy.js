ff7a8814884207aec9ed27508f057c8e03f08d5d
richard-unterberg <richard-unterberg@users.noreply.github.com>
2024-04-18 09:10:32 +0000

    deploy: 3b88d792c32af87b2fbc1cdb89d379478120bb9c

---
+++ assets/batchSamplersUniformGroup-8GnUi2Gy.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-1Wyu1azz.js";import{U as o}from"./colorToUniform-BK_Lmw2w.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
--- assets/batchSamplersUniformGroup-Df3jtgoh.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./index-C6__wYHY.js";import{U as o}from"./colorToUniform-DV9qvp0u.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
