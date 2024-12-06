f392a285dd51b84fae1f933a953e6d4dba98b4af
richard-unterberg <richard-unterberg@users.noreply.github.com>
2024-12-06 09:43:43 +0000

    deploy: acc490db7eec4e3f71ab303a5909d7dad5b835a4

---
+++ assets/batchSamplersUniformGroup-APZW_40D.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-Sl3N3ZkD.js";import{U as o}from"./colorToUniform-DMmoipKj.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
